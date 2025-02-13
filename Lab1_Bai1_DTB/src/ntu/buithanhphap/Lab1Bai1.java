c48849307ef6d2f8a8e42bee24eae40d759b9d5a
phapvuong <btphap.91@gmail.com>
2025-02-13 16:58:06 +0700

    Tao xong du an lab1 bai1

---
+++ Lab1_Bai1_DTB/src/ntu/buithanhphap/Lab1Bai1.java
@@ -0,0 +1,11 @@
+package ntu.buithanhphap;
+
+public class Lab1Bai1 {
+
+	public static void main(String[] args) {
+		// In ra một dòng tiêu đề App
+		System.out.println("Chương trình cho phép nhập họ tên và điểm của sinh viên");
+
+	}
+
+}
