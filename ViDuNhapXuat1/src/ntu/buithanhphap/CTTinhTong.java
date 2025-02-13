b63718f564c3d438d81a85d1f322430141a728e0
phapvuong <btphap.91@gmail.com>
2025-02-13 16:50:13 +0700

    Đã hoàn thành xong chương trình tính tổng

--- ViDuNhapXuat1/src/ntu/buithanhphap/CTTinhTong.java
+++ ViDuNhapXuat1/src/ntu/buithanhphap/CTTinhTong.java
@@ -1,10 +1,31 @@
 package ntu.buithanhphap;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// In ra một dòng tiêu đề App
+		System.out.println("CT TINH TONG");
+		
+		// Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		
+		//In dong huong nhan Moi nhap A
+		System.out.println("Mời nhập a: ");
+		double a = banPhim.nextDouble();
+		
+		//In dong huong nhan Moi nhap B
+		System.out.println("Mời nhập b: ");
+		double b = banPhim.nextDouble();
+		
+		//Tính tổng
+		double tong = a+b;
+		
+		//Xuất
+		System.out.print("Tổng là: ");
+		System.out.print(tong);
+		
 	}
 
 }
