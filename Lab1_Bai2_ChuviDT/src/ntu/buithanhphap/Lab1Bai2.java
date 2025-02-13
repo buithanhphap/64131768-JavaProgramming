3c1ceda944eda00f76e3ca49b368a5f72cc88ae2
phapvuong <btphap.91@gmail.com>
2025-02-13 17:34:13 +0700

    Đã hoàn thiện Lab1 Bai2

--- Lab1_Bai2_ChuviDT/src/ntu/buithanhphap/Lab1Bai2.java
+++ Lab1_Bai2_ChuviDT/src/ntu/buithanhphap/Lab1Bai2.java
@@ -1,9 +1,32 @@
 package ntu.buithanhphap;
 
+import java.util.Scanner;
+
 public class Lab1Bai2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// In ra một dòng tiêu đề App
+		System.out.println("Chương trình tính chu vi, diện tích và cạnh nhỏ nhất của hình chữ nhật");
+		
+		Scanner banPhim = new Scanner(System.in);
+		
+		// Nhập chiều dài
+		System.out.println("Nhập chiều dài: ");
+		double dai = banPhim.nextDouble();
+		
+		// Nhập chiều rộng
+		System.out.println("Nhập chiều rộng: ");
+		double rong = banPhim.nextDouble();
+		
+		// Tính toán
+		double chuVi = (dai + rong) * 2;
+		double dienTich = dai * rong;
+		double canhNho = Math.min(dai, rong);
+		
+		//Xuất
+		System.out.printf("Chu vi: %.1f\n", chuVi);
+		System.out.printf("Diện tích: %.1f\n", dienTich);
+		System.out.printf("Cạnh nhỏ nhất: %.1f\n", canhNho);
 
 	}
 
