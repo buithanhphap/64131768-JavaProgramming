7de144e431c058a6e8d1d05617b060624fec527a
phapvuong <btphap.91@gmail.com>
2025-02-13 17:12:27 +0700

    Đã hoàn thành xong Lab1 bai1

--- Lab1_Bai1_DTB/src/ntu/buithanhphap/Lab1Bai1.java
+++ Lab1_Bai1_DTB/src/ntu/buithanhphap/Lab1Bai1.java
@@ -1,10 +1,25 @@
 package ntu.buithanhphap;
 
+import java.util.Scanner;
+
 public class Lab1Bai1 {
 
 	public static void main(String[] args) {
 		// In ra một dòng tiêu đề App
-		System.out.println("Chương trình cho phép nhập họ tên và điểm của sinh viên");
+		System.out.println("Chương trình cho phép nhập họ và tên, điểm của sinh viên");
+		
+		Scanner banPhim = new Scanner(System.in);
+		
+		// Nhập Họ và tên
+		System.out.print("Họ và tên: ");
+		String hoTen = banPhim.nextLine();
+		
+		// Nhập Điểm TB
+		System.out.print("Điểm TB: ");
+		double diemTB = banPhim.nextDouble();
+		
+		//Xuất thông tin ra màn hình
+		System.out.printf("%s %.1f điểm\n",hoTen, diemTB);	
 
 	}
 
