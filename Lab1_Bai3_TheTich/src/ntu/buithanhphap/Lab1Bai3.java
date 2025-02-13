613fe3793724f45549bf0b5088455ebefa9c627a
phapvuong <btphap.91@gmail.com>
2025-02-13 21:20:38 +0700

    Đã hoàn thành xong Lab1 Bai3

--- Lab1_Bai3_TheTich/src/ntu/buithanhphap/Lab1Bai3.java
+++ Lab1_Bai3_TheTich/src/ntu/buithanhphap/Lab1Bai3.java
@@ -1,9 +1,25 @@
 package ntu.buithanhphap;
 
+import java.util.Scanner;
+
 public class Lab1Bai3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// In ra một dòng tiêu đề App
+		System.out.println("Chương trình tính thể tích của khối lập phương");
+		
+		Scanner banPhim = new Scanner(System.in);
+		
+		// Nhập cạnh của khối lập phương
+		System.out.print("Nhập cạnh của khối lập phương: ");
+		double canh = banPhim.nextDouble();
+		
+		// Tính thể tích
+		double theTich = Math.pow(canh, 3);
+		
+		//Xuất
+		System.out.print("Thể tích của khối lập phương là: ");
+		System.out.print(theTich);
 
 	}
 
