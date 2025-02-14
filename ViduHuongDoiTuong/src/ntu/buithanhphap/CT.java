0f4bde73ce4b6f077dfde4e5105ec7480a94b530
phapvuong <btphap.91@gmail.com>
2025-02-14 19:36:38 +0700

    Đã hoàn thành xong Ví dụ hướng đối tượng với Java và khai thác lớp ArrayList

--- ViduHuongDoiTuong/src/ntu/buithanhphap/CT.java
+++ ViduHuongDoiTuong/src/ntu/buithanhphap/CT.java
@@ -1,9 +1,11 @@
 package ntu.buithanhphap;
 
+import java.util.ArrayList;
+
 public class CT {
 
 	public static void main(String[] args) {
-		// Tạo ra 2 sản phẩm
+		/*// Tạo ra 2 sản phẩm
 		SanPham sp1 = new SanPham();
 		SanPham sp2 = new SanPham(2, "Bánh Bò", "Đồ ăn", "bc.jpg");
 		sp1.setMaSP(1);
@@ -17,7 +19,28 @@ public class CT {
 			   thongtinSP2 += "Tên SP2 là " + sp2.getTenSP();
 			   
 		System.out.println(thongtinSP1);
-		System.out.println(thongtinSP2);
+		System.out.println(thongtinSP2); */
+		
+		//Khai báo danh sách sản phẩm
+		ArrayList<SanPham> dsSanPham;
+		//Xin mới
+		dsSanPham = new ArrayList<SanPham>();
+		//Nhập 3 sản phẩm
+		//SanPham sp1 = new SanPham(0, "Kem", "Giải khát", null);
+		SanPham sp2 = new SanPham(1, "Cơm", "Đồ ăn", null);
+		SanPham sp3 = new SanPham(2, "Phở", "Đồ ăn", null);
+		
+		//Thêm vào ArrayList
+		dsSanPham.add(new SanPham(0, "Kem", "Giải khát", null));
+		dsSanPham.add(sp2);
+		dsSanPham.add(sp3);
+		
+		/*for (SanPham x: dsSanPham)
+			System.out.println(x.toString());*/
+		for (int i=0; i<dsSanPham.size(); i++) {
+			SanPham sp = dsSanPham.get(i);
+			System.out.println(sp.toString());
+		}
 	}
 
 }
