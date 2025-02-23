1295eceaa83df3fe1498c397a8140a3edefb9b90
phapvuong <btphap.91@gmail.com>
2025-02-23 20:53:00 +0700

    Đã hoàn thành xong Lab4_Bai1

---
+++ Lab4_Bai1/src/ntu/buithanhphap/CT.java
@@ -0,0 +1,15 @@
+package ntu.buithanhphap;
+
+public class CT {
+	public static void main(String[] args) {
+		
+		SanPham sp = new SanPham();
+
+        // Nhập thông tin sản phẩm từ người dùng
+        sp.nhap();
+
+        // Xuất thông tin sản phẩm
+        sp.xuat();
+	}
+
+}
